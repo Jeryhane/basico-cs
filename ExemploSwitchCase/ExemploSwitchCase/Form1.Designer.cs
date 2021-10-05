94bb031ddc3147afc5ba91f6679ffb304c4a04b9
Jeryhane Valente <49919226+Jeryhane@users.noreply.github.com>
2021-10-05 15:05:56 -0300

    up new

---
+++ ExemploSwitchCase/ExemploSwitchCase/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ExemploSwitchCase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.cboOperacoes = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe um número:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Informe um número:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Selecione a operação";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN1.Location = new System.Drawing.Point(145, 30);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(121, 26);
+            this.txtN1.TabIndex = 3;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN2.Location = new System.Drawing.Point(145, 68);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(121, 26);
+            this.txtN2.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(92, 169);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(107, 27);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // cboOperacoes
+            // 
+            this.cboOperacoes.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboOperacoes.FormattingEnabled = true;
+            this.cboOperacoes.Items.AddRange(new object[] {
+            "Adição",
+            "Subtração",
+            "Multiplicação",
+            "Divisão",
+            "Nenhum"});
+            this.cboOperacoes.Location = new System.Drawing.Point(145, 107);
+            this.cboOperacoes.Name = "cboOperacoes";
+            this.cboOperacoes.Size = new System.Drawing.Size(121, 28);
+            this.cboOperacoes.TabIndex = 7;
+            this.cboOperacoes.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 227);
+            this.Controls.Add(this.cboOperacoes);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox cboOperacoes;
+    }
+}
+
---
+++ ExemploSwitchCase/ExemploSwitchCase/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploSwitchCase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1 = 0.0, n2 = 0.0, res = 0.0;
+            n1 = Convert.ToDouble(txtN1.Text);
+            n2 = Convert.ToDouble(txtN2.Text);
+
+            MessageBox.Show("Propriedade SelectedItem" + cboOperacoes.SelectedItem);
+            MessageBox.Show("Propriedade SelectedIndex" + cboOperacoes.SelectedIndex);
+
+            switch (cboOperacoes.SelectedIndex)
+            {
+                case 0:
+                    res = n1 + n2;
+                    MessageBox.Show(res.ToString());
+                    break;
+
+                case 1:
+                    res = n1 - n2;
+                    MessageBox.Show(res.ToString());
+                    break;
+
+                case 2:
+                    res = n1 * n2;
+                    MessageBox.Show(res.ToString());
+                    break;
+
+                case 3:
+                    
+                    if (n2 == 0)
+                    {
+                        MessageBox.Show("Tá loca fia?!");
+                        txtN1.Select();//focar na caixa de texto txtN1
+                    } else
+                    {
+                        res = n1 / n2;
+                        MessageBox.Show(res.ToString());
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("Nenhhuma operação selecionada fia!");
+                    break;
+            }
+        }
+    }
+}
